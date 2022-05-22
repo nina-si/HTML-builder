64932a9f1e0686f12851a40a86f064eaa764d818
Nina Sitaeva <sitaeva.nn@gmail.com>
2022-05-22 16:59:07 +0200

    feat: add 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'text.txt');
+
+const stream = fs.createReadStream(filePath, 'utf8');
+
+stream.on('data', (data) => {
+  console.log(data);
+});
