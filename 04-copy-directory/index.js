039ef06fe3d09b85eb9767915206a05fd47ff327
Nina Sitaeva <sitaeva.nn@gmail.com>
2022-05-22 21:45:39 +0200

    feat: add 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const path = require('path');
+const fs = require('fs');
 
+const srcPath = path.join(__dirname, 'files/');
+const destPath = path.join(__dirname, 'files-copy/');
+
+fs.rm(destPath, { recursive: true, force: true }, () => {
+  fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+    if (err) throw err;
+
+    fs.readdir(srcPath, { withFileTypes: true }, (err, files) => {
+      if (err) throw err;
+      files.forEach((file) => {
+        fs.copyFile(
+          path.join(srcPath, file.name),
+          path.join(destPath, file.name),
+          () => {
+            console.log(`File ${file.name} is copied`);
+          }
+        );
+      });
+    });
+  });
+});
